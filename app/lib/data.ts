a4d35e4a59aaef172525061b9b7e389ce888e748
Saddam Ahmed <saddam.groot@gmail.com>
2025-03-18 21:22:16 +0530

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,8 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+//This is the Fetch revenue function
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
