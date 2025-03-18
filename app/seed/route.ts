c151ea88cdeb6975c346577b5cd4c146f5ab3811
Saddam Ahmed <saddam.groot@gmail.com>
2025-03-18 09:25:27 +0530

    changed route.ts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
 
-// import bcrypt from 'bcrypt';
-// import postgres from 'postgres';
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -102,17 +102,17 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
-//   try {
-//     const result = await sql.begin((sql) => [
-//       seedUsers(),
-//       seedCustomers(),
-//       seedInvoices(),
-//       seedRevenue(),
-//     ]);
+export async function GET() {
+  try {
+    // const result = await sql.begin((sql) => [
+    //   seedUsers(),
+    //   seedCustomers(),
+    //   seedInvoices(),
+    //   seedRevenue(),
+    // ]);
 
-//     return Response.json({ message: 'Database seeded successfully' });
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
